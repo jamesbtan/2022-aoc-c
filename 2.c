3ccb453b26031ad26dba1705a5e1f3b9d1cffe4d
James Rupert Tan <jamesruperttan@gmail.com>
2022-12-02 08:41:49 -0500

    Rewrite .c files to switch on whether PART1 is defined to run part 1

--- 2.c
+++ 2.c
@@ -12,11 +12,15 @@ int main(void)
 	char c2;
 	while (fscanf(fp, "%c %c\n", &c1, &c2) != EOF) {
 		// 0 = rock, 1 = paper, 2 = scissors
-		int s1 = c1 - 'A';
 		// 0 = lose, 1 = tie, 2 = win
+		int s1 = c1 - 'A';
+#ifdef PART1
+		int s2 = c2 - 'Z';
+		int v = ((s2 - s1) % 3 + 4) % 3;
+#else
 		int v = c2 - 'X';
-		// int v = ((s2 - s1) % 3 + 4) % 3;
 		int s2 = ((v + 2) % 3 + s1) % 3;
+#endif
 		int s = v * 3 + s2 + 1;
 		ts += s;
 	}
