c957cea0656ebdd71f144ec1d581281e1f854b45
James Rupert Tan <jamesruperttan@gmail.com>
2022-12-02 08:45:51 -0500

    Remove extra 1.c printing

--- 1.c
+++ 1.c
@@ -24,13 +24,6 @@ int main(void)
 	while (fgets(buf, 10, fp) != NULL) {
 		if (strlen(buf) == 1) {
 			qsort(maxes, NUM + 1, sizeof(int), comp);
-			for (int i = 0; i < NUM + 1; i++) {
-				if (i != 0) {
-					printf(" ");
-				}
-				printf("%d", maxes[i]);
-			}
-			printf("\n");
 			maxes[NUM] = 0;
 		} else {
 			int curr = strtol(buf, NULL, 10);
@@ -39,13 +32,8 @@ int main(void)
 	}
 	fclose(fp);
 
-	printf("\n");
 	int tot = 0;
 	for (int i = 0; i < NUM; i++) {
-		if (i != 0) {
-			printf(" ");
-		}
-		printf("%d", maxes[i]);
 		tot += maxes[i];
 	}
 	printf("\n%d\n", tot);
