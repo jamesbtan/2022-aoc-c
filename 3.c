9f8e9b977afd80ad4a2be24c132bcb53694b2962
James Rupert Tan <jamesruperttan@gmail.com>
2022-12-03 00:45:41 -0500

    Day 3

---
+++ 3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int comp(const void *a, const void *b) {
+	char av = *(char *) a;
+	char bv = *(char *) b;
+	return (av > bv) - (bv > av);
+}
+
+int main(void)
+{
+	FILE *fp = fopen("data/input3", "r");
+
+	if (fp == NULL) {
+		return 1;
+	}
+
+	int tp = 0;
+#ifdef PART1
+	char buf[60];
+	while (fgets(buf, 60, fp) != NULL) {
+		int hlen = strlen(buf) / 2;
+		qsort(buf, hlen, sizeof(char), comp);
+		qsort(buf+hlen, hlen, sizeof(char), comp);
+		int i, j;
+		for (i = 0, j = hlen; buf[i] != buf[j];) {
+			if (buf[i] < buf[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		int p = (buf[i] >= 'a') ? buf[i] - 'a' + 1 : buf[i] - 'A' + 27;
+#else
+	char buf[3][60];
+	while (fscanf(fp, "%s\n%s\n%s\n", buf[0], buf[1], buf[2]) != EOF) {
+		for (int i = 0; i < 3; i++) {
+			qsort(buf[i], strlen(buf[i]), sizeof(char), comp);
+		}
+		int ind[3] = {0};
+		while (1) {
+			int same = 1;
+			int min = buf[0][ind[0]];
+			int mini = 0;
+			for (int i = 1; i < 3; i++) {
+				if (buf[0][ind[0]] != buf[i][ind[i]]) {
+					same = 0;
+				}
+				if (buf[i][ind[i]] < min) {
+					min = buf[i][ind[i]];
+					mini = i;
+				}
+			}
+			if (same) break;
+			ind[mini]++;
+		}
+		int p = (buf[0][ind[0]] >= 'a') ? buf[0][ind[0]] - 'a' + 1 : buf[0][ind[0]] - 'A' + 27;
+#endif
+		tp += p;
+	}
+	fclose(fp);
+
+	printf("%d\n", tp);
+}
